brianvans/puppet-gitlab

commit 77d5803ca40d8ca4ab9bca2decb66126cc508ccc

changing how the git package is managed to prevent duplicate declaration conflicts with the rbenv module

manifests/setup.pp
@@ -99,6 +99,14 @@ class gitlab::setup inherits gitlab {
     ensure => '0.6.9.4',
   }
 
+  # git package
+  if ! defined(Package['git']) {
+    package { 'git':
+      name => $git_package_name,
+      ensure => present
+    }
+  }
+
   # other packages
-  ensure_packages([$git_package_name,'postfix','curl'])
+  ensure_packages(['postfix','curl'])
 }

spec/classes/gitlab_setup_spec.rb
@@ -149,9 +149,15 @@ describe 'gitlab' do
       end
       #### Commons packages (all dist.)
       describe 'commons packages' do
-        ['git-core','postfix','curl'].each do |pkg|
+        ['postfix','curl'].each do |pkg|
           it { should contain_package(pkg) }
         end
+      end
+      #### git package
+      describe 'git package' do
+        it { should contain_package('git').with(
+          :name => 'git-core'
+        )}
       end
     end # packages
   end # gitlab::setup

commit 3ce437b9930c20a40da0cffcd57a11964919ceda

fixing style errors, some of which were introduced in 77d5803ca40d8ca4ab9bca2decb66126cc508ccc. doh

manifests/setup.pp
@@ -93,17 +93,20 @@ class gitlab::setup inherits gitlab {
   }
 
   rbenv::gem { 'charlock_holmes':
+    ensure => '0.6.9.4',
     user   => $git_user,
     home   => $git_home,
     ruby   => $rbenv_ruby_version,
-    ensure => '0.6.9.4',
   }
+
+  # spaceship hackery to work around https://github.com/alup/puppet-rbenv/issues/38
+  Rbenv::Gem<| |> ~> Exec["rbenv::rehash ${git_user} ${rbenv_ruby_version}"]
 
   # git package
   if ! defined(Package['git']) {
     package { 'git':
-      name => $git_package_name,
-      ensure => present
+      ensure => present,
+      name   => $git_package_name
     }
   }
 